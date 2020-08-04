c70848b8c822696b8cb624bbdaf127a3b69f9d02
yang <1372201820@qq.com>
2020-08-04 14:13:36 +0800

    完成首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,65 +1,62 @@
+import {request} from "../../request/index.js"
 wx-Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    
+    //轮播图数组
+    swiperList:[],
+    //导航数据
+    catesList:[],
+    //楼层数据
+    floorList:[]
   },
 
   /**
-   * 生命周期函数--监听页面加载
+   * 生命周期函数--监听页面加载 create
    */
   onLoad: function (options) {
-    
+    this.getSwiperList(),
+    this.getCatesList(),
+    this.getFloorList()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
+  //获取轮播图数据
+  getSwiperList(){
+      //获取轮播图  修改为promise
+      // request({
+      //   url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+      //   success: (result) => {
+      //     this.setData({
+      //       swiperList:result.data.message
+      //     })
+      //   },
+      // })
+      request({
+        url:'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata', 
+      }).then(result=>{
+        this.setData({
+          swiperList:result.data.message
+        })
+      })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
+  //获取首页导航数据
+  getCatesList(){
+    request({
+      url:'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems', 
+    }).then(result=>{
+      this.setData({
+        catesList:result.data.message
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  //获取楼层数据
+  getFloorList(){
+    request({
+      url:'https://api-hmugo-web.itheima.net/api/public/v1/home/floordata', 
+    }).then(result=>{
+      this.setData({
+        floorList:result.data.message
+      })
+    })
   }
 })
