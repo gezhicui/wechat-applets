cb3e6ade639675582cddb1aacca4be1ece56852e
yang <1372201820@qq.com>
2020-08-05 12:14:37 +0800

    添加分类页面及加入本地缓存

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,63 @@
 // pages/category/index.js
+import {request} from "../../request/index.js"
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    //左侧菜单数据
+    leftMenuList:[],
+    //右侧数据
+    rightContent:[],
+    //被点击的左侧菜单索引
+    currentIndex:0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  Cates:[],
   onLoad: function (options) {
-
+    //获取本地存储中有旧的数据
+    const Cates = wx.getStorageSync('cates');
+    //判断
+    if(!Cates){
+      this.getCates()
+    }else{
+      //有旧的数据  定义一个过期时间 这里为了调试设置为1秒
+      if(Date.now()-Cates.time>1000){
+        this.getCates()
+      }else{
+        // console.log("旧数据");
+        this.Cates=Cates.data
+        let leftMenuList=this.Cates.map(v=>v.cat_name);
+        let rightContent=this.Cates[0].children;
+        this.setData({
+          leftMenuList,
+          rightContent
+        })
+      }
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  //获取分类数据
+  getCates(){
+    request({
+      url:'https://api-hmugo-web.itheima.net/api/public/v1/categories', 
+    }).then(result=>{
+      this.Cates=result.data.message;
+      //把接口数据存入到本地存储中
+      wx.setStorageSync('cates', {time:Date.now(),data:this.Cates})
+      let leftMenuList=this.Cates.map(v=>v.cat_name);
+      let rightContent=this.Cates[0].children;
+      this.setData({
+        leftMenuList,
+        rightContent
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //点击切换左侧按钮
+  handleItemTep(e){
+    //获取被激活的索引
+    const {index} = e.currentTarget.dataset
+    let leftMenuList=this.Cates.map(v=>v.cat_name);
+    let rightContent=this.Cates[index].children;
+    this.setData({
+      currentIndex : index,
+      leftMenuList,
+      rightContent
+    })
   }
 })
